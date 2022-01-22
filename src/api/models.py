63c8f45ba3080f372837f819550c5af3b3b1ed70
Yevhenbk <balagutrak.jenia@gmail.com>
2022-01-22 16:57:24 +0000

    server-side

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,33 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.dialects.postgresql import VARCHAR
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Account(db.Model):
+    __tablename__="account"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    password = db.Column(db.String(280), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Account %r>' % self.email
 
-    def serialize(self):
+    def to_dict(self):
         return {
             "id": self.id,
             "email": self.email,
+            "password": self.password
             # do not serialize the password, its a security breach
         }
+
+    @classmethod
+    def get_by_email(cls, email):
+        account = cls.query.filter_by(email=email).one_or_none()
+        return account
+
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,8 +2,12 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from sqlalchemy import exc
+from api.models import db, Account
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+
+from werkzeug.security import check_password_hash, generate_password_hash
 
 api = Blueprint('api', __name__)
 
@@ -16,3 +20,56 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@api.route('/login', methods=['POST'])
+def login():
+    email = request.json.get(
+        'email', None
+    )
+    password = request.json.get(
+        'password', None
+    )
+    if not (email and password):
+        return ({'error': 'Wrong email or password'}), 400
+
+    account = Account.get_by_email(email)
+    print(account)
+    if not account: return ({'error': 'usuario no encontrado'}), 400
+
+ 
+    if account.is_active and check_password_hash(account.password, password):
+        token = create_access_token(identity=account.id, expires_delta=timedelta(minutes=120))
+        return {'token': token, "email":account.email}, 200
+
+    return ({'error': 'Wrong email or password'}), 400
+
+
+@api.route('/signup', methods=['POST'])
+def create_account(): 
+    email = request.json.get(
+        'email', None
+    )
+    password = request.json.get(
+        'password', None
+    ) 
+    
+    print(email, password, "###################################")
+    if not (password and email):
+        return ({'error': 'Some fields are missing'}), 400
+    
+    
+    account = Account(
+        email=email, 
+        password=generate_password_hash(password, method='pbkdf2:sha256', salt_length=16),
+        is_active=True
+    )
+    print(account, "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+    
+    
+    try:
+        account.create()
+        return jsonify(account.to_dict()), 201
+        
+    except exc.IntegrityError:
+        return ({'error': 'This email / phone number is already in use'}), 400
